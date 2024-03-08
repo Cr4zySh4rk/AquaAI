5d1ae97fb526244132998ccada8818ac357ce608
Adithya Manjunath <adithyam211@gmail.com>
2024-03-08 16:50:12 +0530

    Update launcher.sh

--- Scripts/launcher.sh
+++ Scripts/launcher.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # launcher.sh
-sudo -u pi tmux new-session -d -s aquaai python /home/pi/AquaAI.py
-sudo -u pi tmux set -g status off
+sudo tmux new-session -d -s aquaai python /home/pi/AquaAI.py
+sudo tmux set -g status off
