2eeff65b57555407adfe14463601fcbcac652b79
Adithya Manjunath <adithyam211@gmail.com>
2024-03-13 20:24:16 +0530

    Update launcher.sh

--- Scripts/launcher.sh
+++ Scripts/launcher.sh
@@ -4,7 +4,7 @@
 # Start a new tmux session in detached mode with the name 'aquaai' and run the AquaAI.py script using Python
 sudo tmux new-session -d -s aquaai python /home/pi/AquaAI.py
 pm2 start /home/pi/Web/Server/server.js
-sudo tmux new-session -d -s reactserver npm start /home/pi/Web/Client
+sudo tmux new-session -d -s reactserver sudo PORT=80 npm start /home/pi/Web/Client
 
 # Disable status line in the tmux session
 sudo tmux set -g status off
