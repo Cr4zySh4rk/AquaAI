c50bca9a4c0123d611a0801cc6ac23b7d3b3d165
AkshayPrasad25 <akshayprasad510@gmail.com>
2024-03-18 12:12:53 +0530

    Final Changes

--- Web/Client/src/components/AnalyticData.js
+++ Web/Client/src/components/AnalyticData.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import Chart from 'chart.js/auto';
-import './analytic.css'
+import './analytic.css';
 import { TbGridDots } from "react-icons/tb";
 import { TbDeviceAnalytics } from "react-icons/tb";
 import { MdOutlineAppSettingsAlt } from "react-icons/md";
@@ -13,40 +13,44 @@ import { LuBrainCircuit } from "react-icons/lu";
 const AnalyticData = () => {
   const chartRef = useRef([]);
   const [uptime, setUptime] = useState("Loading...");
-
+  const [selectedRange, setSelectedRange] = useState("Day");
 // eslint-disable-next-line
-  const fetchDataAndUpdateCharts = async (csvFilePath) => {
+  const fetchDataAndUpdateCharts = async () => {
     try {
       const response = await axios.get('http://192.168.4.1:3001/analytics');
       const jsonData = response.data;
-      updateChart('soilChart', 'Soil_Moisture', 'Soil Moisture', 'rgba(105, 89, 62, 1)', jsonData, "%");
-      updateChart('temperatureChart', 'Temperature', 'Temperature', 'rgba(255, 99, 132, 1)', jsonData, "°C");
-      updateChart('humidityChart', 'Humidity', 'Humidity', 'rgba(255, 99, 71, 1)', jsonData, "%");
-      updateChart('nitrogenChart', 'Nitrogen', 'Nitrogen', 'rgba(30, 144, 255, 1)', jsonData, "mg/kg");
-      updateChart('phosphorousChart', 'Phosphorus', 'Phosphorus', 'rgba(255, 215, 0, 1)', jsonData, "mg/kg");
-      updateChart('potassiumChart', 'Potassium', 'Potassium', 'rgba(128, 0, 128, 1)', jsonData, "mg/kg");
-      updateChart('waterDispensedChart', 'Water_Dispensed', 'Water Dispensed', 'rgba(75, 192, 192, 1)', jsonData, "mL");
+      const numberOfEntries = getNumberOfEntries(selectedRange);
+      updateChart('soilChart', 'Soil_Moisture', 'Soil Moisture', 'rgba(105, 89, 62, 1)', jsonData.slice(-numberOfEntries), "%");
+      updateChart('temperatureChart', 'Temperature', 'Temperature', 'rgba(255, 99, 132, 1)', jsonData.slice(-numberOfEntries), "°C");
+      updateChart('humidityChart', 'Humidity', 'Humidity', 'rgba(255, 99, 71, 1)', jsonData.slice(-numberOfEntries), "%");
+      updateChart('nitrogenChart', 'Nitrogen', 'Nitrogen', 'rgba(30, 144, 255, 1)', jsonData.slice(-numberOfEntries), "mg/kg");
+      updateChart('phosphorousChart', 'Phosphorus', 'Phosphorus', 'rgba(255, 215, 0, 1)', jsonData.slice(-numberOfEntries), "mg/kg");
+      updateChart('potassiumChart', 'Potassium', 'Potassium', 'rgba(128, 0, 128, 1)', jsonData.slice(-numberOfEntries), "mg/kg");
+      updateChart('waterDispensedChart', 'Water_Dispensed', 'Water Dispensed', 'rgba(75, 192, 192, 1)', jsonData.slice(-numberOfEntries), "mL");
     } catch (error) {
       console.error('Error fetching or processing data:', error);
     }
   };
+
   useEffect(() => {
-    const csvFilePath = 'data.csv';
-    fetchDataAndUpdateCharts(csvFilePath);
+    fetchDataAndUpdateCharts();
 
     const intervalId = setInterval(() => {
-      fetchDataAndUpdateCharts(csvFilePath);
+      fetchDataAndUpdateCharts();
     }, 10000);
 
     return () => clearInterval(intervalId);
   }, [fetchDataAndUpdateCharts]);
 
+  useEffect(() => {
+    fetchDataAndUpdateCharts('data.csv');
+  }, [selectedRange, fetchDataAndUpdateCharts]);
+
   const updateChart = (containerId, dataKey, yAxisLabel, color, jsonData, unit) => {
     let data = [];
     let labels = [];
-    let startIndex = Math.max(0, jsonData.length - 15);
 
-    for (let i = startIndex; i < jsonData.length; i++) {
+    for (let i = 0; i < jsonData.length; i++) {
       let value = parseFloat(jsonData[i][dataKey]);
       data.push(value);
       labels.push(i + 1);
@@ -132,7 +136,20 @@ const AnalyticData = () => {
       });
     }
   };
-  
+
+  const getNumberOfEntries = (range) => {
+    switch (range) {
+      case "Day":
+        return 15;
+      case "Week":
+        return 100;
+      case "Month":
+        return 500;
+      default:
+        return 15;
+    }
+  };
+
   const fetchUptime = async () => {
     try {
       const response = await axios.get('http://192.168.4.1:3001/uptime');
@@ -143,7 +160,7 @@ const AnalyticData = () => {
       setUptime('Error');
     }
   };
-  
+
   useEffect(() => {
     const uptimeIntervalId = setInterval(() => {
       fetchUptime();
@@ -153,6 +170,10 @@ const AnalyticData = () => {
       clearInterval(uptimeIntervalId);
     };
   }, []);
+
+  const handleRangeChange = (event) => {
+    setSelectedRange(event.target.value);
+  };
 
   return (
     <div className='analytic-wrapper'>
@@ -171,7 +192,16 @@ const AnalyticData = () => {
       </div>
       <div className='analytic-cover'>
         <div className='analytic-cont'>
-          <h1>Analytics:</h1>
+          <div className='hero-crumb'>
+            <h1>Analytics:</h1>
+            <div className='select'>
+              <select name="Range" id="Range" onChange={handleRangeChange}>
+                <option value="Day">Day</option>
+                <option value="Week">Week</option>
+                <option value="Month">Month</option>
+              </select>
+            </div>
+          </div>
           <div className='chart-container'>
             <canvas id='soilChart'></canvas>
           </div>
